9c82cc63e5313e2ff74bc2cbd35f88fe376c7092
Ryan McQuen <rpcm@linux.com>
2020-12-27 15:25:27 -0800

    Getting so close ...
    Signed-off-by: Ryan McQuen <rpcm@linux.com>

--- src/index.js
+++ src/index.js
@@ -1,33 +1,27 @@
-import * as fs from "fs";
+import { promises as fs } from "fs";
 
 module.exports = {
     disposable: null,
-    bookMap: {},
-    noteMap: {},
-    writeNote(path, title, body) {
-        fs.mkdir(path, { recursive: true }, (err) => {
-            if (err) {
-                throw err;
-            }
-            fs.writeFile(`${path}/${title}.md`, body, (err) => {
-                if (err) {
-                    return console.log(err);
-                }
-            });
-        });
+    dataMap: { books: {}, notes: {} },
+    getDataMapPath: (plainTextPath) => {
+        return `${plainTextPath}/.__DATA_MAP__.json`;
     },
-    async getMapsAndWriteAllNotes(plainTextPath) {
+    async writeNote(path, title, body) {
+        await fs.mkdir(path, { recursive: true });
+        await fs.writeFile(`${path}/${title}.md`, body);
+    },
+    async getDataAndWriteAllNotes(plainTextPath) {
         // Sync everything one time:
         const allNotes = await this.disposable.notes.all();
 
         return new Promise(async (resolve, reject) => {
             await Promise.all(
                 await allNotes.docs.map(async (doc) => {
-                    this.noteMap[doc._id] = doc.title;
+                    this.dataMap.notes[doc._id] = doc.title;
                     const bookData = await this.disposable.books.get(
                         doc.bookId
                     );
-                    this.bookMap[doc.bookId] = bookData.name;
+                    this.dataMap.books[doc.bookId] = bookData.name;
 
                     this.writeNote(
                         `${plainTextPath}/${bookData.name}`,
@@ -37,24 +31,16 @@ module.exports = {
                 })
             );
 
-            resolve([this.bookMap, this.noteMap]);
+            resolve(this.dataMap);
         });
     },
-    writeMaps(plainTextPath, maps) {
-        fs.mkdir(plainTextPath, { recursive: true }, (err) => {
-            if (err) {
-                throw err;
-            }
-            fs.writeFile(
-                `${plainTextPath}/.__DATA_MAP__.json`,
-                JSON.stringify(maps),
-                (err) => {
-                    if (err) {
-                        return console.log(err);
-                    }
-                }
-            );
-        });
+    async writeMaps(plainTextPath, maps) {
+        await fs.mkdir(plainTextPath, { recursive: true });
+
+        await fs.writeFile(
+            this.getDataMapPath(plainTextPath),
+            JSON.stringify(maps)
+        );
     },
     async activate() {
         if (inkdrop && !inkdrop.isMobile) {
@@ -62,34 +48,80 @@ module.exports = {
             if (backupPath) {
                 const plainTextPath = `${backupPath}/PLAIN_TEXT`;
                 this.disposable = inkdrop.main.dataStore.getLocalDB();
-                const maps = await this.getMapsAndWriteAllNotes(plainTextPath);
-                this.writeMaps(plainTextPath, maps);
+                await this.getDataAndWriteAllNotes(plainTextPath);
+                this.writeMaps(plainTextPath, this.dataMap);
                 // Sync stuff on changes:
-                this.disposable.onChange((change) => {
+                this.disposable.onChange(async (change) => {
                     try {
                         const typeOfChange = change.id.split(":")[0];
 
                         switch (typeOfChange) {
                             case "note":
                                 const bookPath = `${plainTextPath}/${
-                                    this.bookMap[change.doc.bookId]
+                                    this.dataMap.books[change.doc.bookId]
                                 }`;
+
+                                // If the title has changed, rename the old note.
+                                if (
+                                    change.doc.title !==
+                                    this.dataMap.notes[change.id]
+                                ) {
+                                    const oldDataMap = JSON.parse(
+                                        await fs.readFile(
+                                            this.getDataMapPath(plainTextPath),
+                                            "utf8"
+                                        )
+                                    );
+
+                                    await fs.rename(
+                                        `${bookPath}/${
+                                            oldDataMap.notes[change.id]
+                                        }.md`,
+                                        `${bookPath}/${change.doc.title}.md`
+                                    );
+                                    this.dataMap.notes[change.id] =
+                                        change.doc.title;
+                                }
                                 this.writeNote(
                                     bookPath,
                                     change.doc.title,
                                     change.doc.body
                                 );
+                                this.writeMaps(plainTextPath, this.dataMap);
 
                                 break;
-                            // case "book":
-                            //     fs.rename("/tmp/hello", "/tmp/world", (err) => {
-                            //         if (err) throw err;
-                            //         console.log("renamed complete");
-                            //     });
-                            //     break;
+                            case "book":
+                                debugger;
+                                if (
+                                    change.doc.name !==
+                                    this.dataMap.books[change.id]
+                                ) {
+                                    const oldDataMap = JSON.parse(
+                                        await fs.readFile(
+                                            this.getDataMapPath(plainTextPath),
+                                            "utf8"
+                                        )
+                                    );
+                                    debugger;
+
+                                    await fs.rename(
+                                        `${plainTextPath}/${
+                                            oldDataMap.books[change.id]
+                                        }`,
+                                        `${plainTextPath}/${
+                                            this.dataMap.books[change.id]
+                                        }`
+                                    );
+                                    this.dataMap.books[change.id] =
+                                        change.doc.name;
+                                }
+                                break;
                         }
                     } catch (err) {
-                        console.warn("Plain text backup failed:", err);
+                        console.warn(
+                            "Plain text backup may have had an issue:",
+                            err
+                        );
                     }
                 });
             }
